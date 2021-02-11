1b01db4c7ba186d27154dd1748b8b224f2647a50
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2021-02-11 15:02:02 +0300

    Added autowiring and autoconfiguration to MyBusinessFeature/di.php

--- src/MyBusinessFeature/di.php
+++ src/MyBusinessFeature/di.php
@@ -7,7 +7,12 @@ namespace App\MyBusinessFeature;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $di): void {
-    $services = $di->services();
+    $services = $di
+        ->services()
+        ->defaults()
+        ->autowire()
+        ->autoconfigure()
+    ;
 
     $services->set('my_business_feature.some_service', \stdClass::class);
 };
