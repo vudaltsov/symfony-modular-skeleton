d9b36a4e84b70ea991816bfc4cf7f84dbfe6567d
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2023-10-20 14:47:58 +0300

    Skeleton 6.3

--- src/Kernel.php
+++ src/Kernel.php
@@ -3,27 +3,9 @@
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
-use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
-final class Kernel extends BaseKernel
+class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
-
-    protected function configureContainer(ContainerConfigurator $container): void
-    {
-        $container->import('../config/{packages}/*.yaml');
-        $container->import("../config/{packages}/{$this->environment}/*.yaml");
-        $container->import('./**/{di}.php');
-        $container->import("./**/{di}_{$this->environment}.php");
-    }
-
-    protected function configureRoutes(RoutingConfigurator $routes): void
-    {
-        $routes->import("../config/{routes}/{$this->environment}/*.yaml");
-        $routes->import('../config/{routes}/*.yaml');
-        $routes->import("./**/{routing}_{$this->environment}.php");
-        $routes->import('./**/{routing}.php');
-    }
 }
